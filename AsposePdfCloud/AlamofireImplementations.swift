e310bc4dd61282f20d1f752c5a9108c50cdb7b19
Andrey Kaferi <andrey.kaferi@aspose.com>
2019-11-26 16:38:49 +0200

    Minor fix

--- AsposePdfCloud/AlamofireImplementations.swift
+++ AsposePdfCloud/AlamofireImplementations.swift
@@ -45,8 +45,7 @@ extension Request {
 struct ArrayEncoding: ParameterEncoding {
     func encode(_ urlRequest: URLRequestConvertible, with parameters: Parameters?) throws -> URLRequest {
         var request = try URLEncoding().encode(urlRequest, with: parameters)
-        request.url = URL(string: request.url!.absoluteString
-            .replacingOccurrences(of: "%5B%5D=", with: "="))
+        request.url = URL(string: request.url!.absoluteString.replacingOccurrences(of: "%5B%5D=", with: "="))
         return request
     }
 }
